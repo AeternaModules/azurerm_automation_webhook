b7c90207e359af41e58da6d8f3fb8509f2354403
Daniel Allen <dan@aeternagroup.net>
2026-07-12 21:57:52 +0100

    Update to schema version v4.80.0

--- main.tf
+++ main.tf
@@ -1,3 +1,8 @@
+data "azurerm_key_vault_secret" "uri" {
+  for_each     = { for k, v in var.automation_webhooks : k => v if v.uri_key_vault_id != null && v.uri_key_vault_secret_name != null }
+  name         = each.value.uri_key_vault_secret_name
+  key_vault_id = each.value.uri_key_vault_id
+}
 resource "azurerm_automation_webhook" "automation_webhooks" {
   for_each = var.automation_webhooks
 
@@ -9,6 +14,6 @@ resource "azurerm_automation_webhook" "automation_webhooks" {
   enabled                 = each.value.enabled
   parameters              = each.value.parameters
   run_on_worker_group     = each.value.run_on_worker_group
-  uri                     = each.value.uri
+  uri                     = each.value.uri != null ? each.value.uri : try(data.azurerm_key_vault_secret.uri[each.key].value, null)
 }
 
--- variables.tf
+++ variables.tf
@@ -12,18 +12,22 @@ Optional:
     - parameters
     - run_on_worker_group
     - uri
+    - uri_key_vault_id (alternative to uri - read from Key Vault instead)
+    - uri_key_vault_secret_name (alternative to uri - read from Key Vault instead)
 EOT
 
   type = map(object({
-    automation_account_name = string
-    expiry_time             = string
-    name                    = string
-    resource_group_name     = string
-    runbook_name            = string
-    enabled                 = optional(bool) # Default: true
-    parameters              = optional(map(string))
-    run_on_worker_group     = optional(string)
-    uri                     = optional(string)
+    automation_account_name   = string
+    expiry_time               = string
+    name                      = string
+    resource_group_name       = string
+    runbook_name              = string
+    enabled                   = optional(bool) # Default: true
+    parameters                = optional(map(string))
+    run_on_worker_group       = optional(string)
+    uri                       = optional(string)
+    uri_key_vault_id          = optional(string)
+    uri_key_vault_secret_name = optional(string)
   }))
   validation {
     condition = alltrue([
